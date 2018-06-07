52db9ba337757b7467054325cbeb8b8765ff38f3
Evan Hobbs <evan.hobbs1988@gmail.com>
2018-06-07 00:29:16 -0500

    Add files via upload

---
+++ functions basics 2.py
@@ -0,0 +1,46 @@
+#question: Countdown - Create a function that accepts a number as an input.  Return a new array that counts down by one, from the number (as arrays 'zero'th element) down to 0 (as the last element).  For example countDown(5) should return [5,4,3,2,1,0].
+def countdown(start):
+    return list(range(start,0,-1))
+#Print and Return - Your function will receive an array with two numbers. Print the first value, and return the second
+def function_that_prints():
+    print "print"
+
+
+def function_that_returns():
+    return "return"
+
+
+f1 = function_that_prints()
+f2 = function_that_returns()
+#First Plus Length - Given an array, return the sum of the first value in the array, plus the array's length.
+    arr = []
+    return arr(0)+arr.length
+#Values Greater than Second - Write a function that accepts any array, and returns a new array with the array values that are greater than its 2nd value.  
+#Print how many values this is.  
+#If the array is only element long, have the function return False
+functions(arr):
+
+   if arr(2)<i:
+       add i to NewList:
+
+# This Length, That Value - Given two numbers, return array of length num1 with each value num2.  
+# Print "Jinx!" if they are same.
+def getCount(arr, n, num1, num2):
+
+    # Find num1
+    for i in range(0, n):
+        if (arr[i] == num1):
+            break
+
+
+    # Find num2
+    for j in range(n-1, i+1, -1):
+        if (arr[j] == num2):
+            break
+
+    # If num2 is equal jinx
+    if (j == i):
+        return "jinx!"
+
+    # return the two elements AND jinx.
+    return (j - i - 1)
